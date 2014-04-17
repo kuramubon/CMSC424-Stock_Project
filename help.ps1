e9c5b8a33263bab147cf504d8481b05d6bfe8822
eddiedavis <eddiedavis004@gmail.com>
2014-04-17 02:40:22 -0400

    company CSVs are smaller, history.csv contains historical prices, help1.ps creates history.csv

---
+++ help.ps1
@@ -0,0 +1,11 @@
+﻿$res = 'history.csv'
+'Company_ID, Date, Close' > $res
+
+foreach ($file in dir *.csv)
+{
+  if ($file -notmatch '(\w+).csv') { continue }
+
+  $csv = Import-Csv $file
+  $csv | Foreach {"{0},{1},{2}" -f `
+    $matches[1], $_.Date, $_.Close} >> $res
+}
