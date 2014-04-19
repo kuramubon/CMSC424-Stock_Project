50db5683ab0d5eee56378749b84fe09d5c9b6be9
eddiedavis <eddiedavis004@gmail.com>
2014-04-19 14:43:47 -0400

    made a bit more autonomous

--- help.ps1
+++ help.ps1
@@ -1,11 +1,20 @@
-﻿$res = 'history.txt'
-'Company_ID, Date, Stock_Price' > $res
+﻿Gci *.csv | select -expand Fullname | %{
+    $current = import-csv $_
+    $newname = $_ -replace ".csv", "trimmed.tx"
+    $current | select date, "Adj Close" | export-csv $newname -notype
+}
 
-foreach ($file in dir *.csv)
+rm *.csv
+
+New-Item "history.csv" -type file
+
+foreach ($file in dir *.tx)
 {
-  if ($file -notmatch '(\w+).csv') { continue }
+  if ($file -notmatch '(\w+)trimmed.tx') { continue }
 
   $csv = Import-Csv $file
   $csv | Foreach {"{0},{1},{2}" -f `
     $matches[1], $_.Date, $_."Adj Close"} >> $res
 }
+
+rm *.tx
